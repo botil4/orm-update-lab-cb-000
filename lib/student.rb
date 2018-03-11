52c842587022b047502faad950192c5a0727e7a9
Travis Clarkson <tk1667@gmail.com>
2018-03-11 02:06:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,8 +56,12 @@ class Student
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
 
-  end
+    Student.new_from_db(DB[:conn].execute(sql, name))
+    end
 
   def update
 
